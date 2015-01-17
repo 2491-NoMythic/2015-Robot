a6bfb27613fdad27bd990ef3671ad99df381567e
DConnorA <dalvarez@greatriverschool.org>
2015-01-17 14:58:33 -0800

    Added Constants and Drivetrain subsystem

---
+++ src/com/_2491nomythic/settings/Constants.java
@@ -0,0 +1,6 @@
+package com._2491nomythic.settings;
+
+public class Constants {
+	//Ports and frequently used numbers here (or strings?)
+	
+}
---
+++ src/com/_2491nomythic/subsystems/Drivetrain.java
@@ -0,0 +1,27 @@
+package com._2491nomythic.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Drivetrain extends Subsystem {
+	Motor frontRight, frontCenter, frontLeft, backRight, backCenter, backLeft;
+	Encoder encoderLeft, encoderCenter, encoderRight;
+	
+	Drivetrain() {
+		
+	}
+	
+	
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
