5c50cbb9413249f55592fb891402d77b0a93765d
No Mythic Lenovo 1 <2491nomythic@gmail.com>
2015-01-17 14:39:18 -0600

    Actually fixed error in JoystickAxisButton

--- src/com/_2491nomythic/util/JoystickAxisButton.java
+++ src/com/_2491nomythic/util/JoystickAxisButton.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.nomythic.util;
+package com._2491nomythic.util;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.buttons.Button;
