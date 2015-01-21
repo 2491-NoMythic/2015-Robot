652ede4db10b6700b3f736a0e5195f4f24176b2a
ghcat <ghcat123@gmail.com>
2015-01-20 19:05:16 -0600

    Added Arm Subsystem and Variables

---
+++ src/com/_2491nomythic/helios/settings/Variables.java
@@ -0,0 +1,7 @@
+package com._2491nomythic.helios.settings;
+
+public class Variables {
+	public static double armPID_P = 1.0;
+	public static double armPID_I = 0.0;
+	public static double armPID_D = 0.0;
+}
