ac01ac31547c4e59b40ded1228c6430f7bfd8f20
No Mythic Lenovo 1 <2491nomythic@gmail.com>
2015-01-20 18:13:46 -0600

    mapped elevator and dreambelt controls

--- src/com/_2491nomythic/helios/settings/HandheldControllerMap.java
+++ src/com/_2491nomythic/helios/settings/HandheldControllerMap.java
@@ -5,14 +5,26 @@ public class HandheldControllerMap {
 	public static final String controllerType = "HandheldController";
 
 	//Axes
-	//Joystick 0: Drive
+	//Left Joystick on Controller 0: Drive
 	public static final int DriveController = 0;
 	public static final int DriveAxisX = 1;
 	public static final int DriveAxisY = 2;
-	//Joystick 0: Turn
+	//Right X Axis on Controller 0: Turn
 	public static final int TurnController = 0;
 	public static final int TurnAxis = 3;
+	//Right Y Axis on Controller 1: Elevator
+	public static final int ElevatorController = 1;
+	public static final int ElevatorAxis = 4;
+	//Left Y Axis on Controller 1: DreamBelt
+	public static final int DreamBeltController = 1;
+	public static final int DreamBeltAxis = 2;
+	//Up & Down D-Pad on Controller 1: DreamBelt Turn
+	public static final int DreamBeltTurnController = 1;
+	public static final int DreamBeltTurnAxis = 6;
 
 	//Buttons
-
+	//LB Button on Controller 1: DreamBelt Grab
+	public static final int DreamBeltGrabController = 1;
+	public static final int DreamBeltGrabButton = 5;
+	
 }
