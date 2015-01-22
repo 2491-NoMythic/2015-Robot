eb998dbd38f86b0c2ae4e01e2bd6d600557930f2
No Mythic Lenovo 1 <2491nomythic@gmail.com>
2015-01-22 17:34:24 -0600

    Created/ Finished reset encoder commands

---
+++ src/com/_2491nomythic/helios/commands/ResetCenterEncoder.java
@@ -0,0 +1,47 @@
+package com._2491nomythic.helios.commands;
+
+import com._2491nomythic.helios.commands.CommandBase;
+
+/**
+ *
+ */
+public class ResetCenterEncoder extends CommandBase {
+	
+	private boolean done = false;
+
+    public ResetCenterEncoder() {
+    	requires(drivetrain);
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	
+    	drivetrain.getCenterEncoder().reset();
+    	done = true;
+    	
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return done;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	done = false;
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	
+    	end();
+    }
+}
