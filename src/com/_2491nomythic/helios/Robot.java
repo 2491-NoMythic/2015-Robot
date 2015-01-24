8bf5edad61533f15f68ff8b83495eca5f559dd88
Tellow Krinkle <etang110@gmail.com>
2015-01-24 12:00:24 -0600

    Fixing issues
    The robot won’t run with everything set to 0.

--- src/com/_2491nomythic/helios/Robot.java
+++ src/com/_2491nomythic/helios/Robot.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
+import com._2491nomythic.helios.commands.CommandBase;
 import com._2491nomythic.helios.commands.ExampleCommand;
 import com._2491nomythic.helios.subsystems.ExampleSubsystem;
 
@@ -29,6 +30,7 @@ public class Robot extends IterativeRobot {
      */
     public void robotInit() {
 		oi = new OI();
+		CommandBase.init();
         // instantiate the command used for the autonomous period
         autonomousCommand = new ExampleCommand();
     }
--- src/com/_2491nomythic/helios/settings/Constants.java
+++ src/com/_2491nomythic/helios/settings/Constants.java
@@ -6,18 +6,18 @@ public class Constants {
 	public static final int ControllerTwoPort = 2;
 	
 	//Drive
-	public static final int driveTalonFrontLeftChannel = 0;
-	public static final int driveTalonFrontCenterChannel = 0;
-	public static final int driveTalonFrontRightChannel = 0;
-	public static final int driveTalonBackLeftChannel = 0;
-	public static final int driveTalonBackCenterChannel = 0;
-	public static final int driveTalonBackRightChannel = 0;
+	public static final int driveTalonFrontLeftChannel = 10;
+	public static final int driveTalonFrontCenterChannel = 14;
+	public static final int driveTalonFrontRightChannel = 12;
+	public static final int driveTalonBackLeftChannel = 11;
+	public static final int driveTalonBackCenterChannel = 15;
+	public static final int driveTalonBackRightChannel = 13;
 	public static final int driveEncoderLeftAChannel = 0;
-	public static final int driveEncoderLeftBChannel = 0;
-	public static final int driveEncoderCenterAChannel = 0;
-	public static final int driveEncoderCenterBChannel = 0;
-	public static final int driveEncoderRightAChannel = 0;
-	public static final int driveEncoderRightBChannel = 0;
+	public static final int driveEncoderLeftBChannel = 1;
+	public static final int driveEncoderCenterAChannel = 4;
+	public static final int driveEncoderCenterBChannel = 5;
+	public static final int driveEncoderRightAChannel = 2;
+	public static final int driveEncoderRightBChannel = 3;
 	public static final boolean driveEncoderLeftReversed = false;
 	public static final boolean driveEncoderCenterReversed = false;
 	public static final boolean driveEncoderRightReversed = false;
@@ -25,22 +25,22 @@ public class Constants {
 	
 	
 	//Arm
-	public static final int armTalonArmMotorLeftChannel = 0;
-	public static final int armTalonArmMotorRightChannel = 0;
+	public static final int armTalonArmMotorLeftChannel = 16;
+	public static final int armTalonArmMotorRightChannel = 17;
 	public static final int ArmBrakeOnChannel = 0;
-	public static final int ArmBrakeOffChannel = 0;
+	public static final int ArmBrakeOffChannel = 1;
 	public static final double ArmBrakeMaxSpeed = 10.0; //Max speed of arm to allow brake to be engaged
-	public static final int armEncoderAChannel = 0;
-	public static final int armEncoderBChannel = 0;
+	public static final int armEncoderAChannel = 6;
+	public static final int armEncoderBChannel = 7;
 	public static final boolean armEncoderReversed = false;
 	public static final double armEncoderToDegrees = 1.0;
 	public static final double armMinPosition = 0.0;
 	public static final double armMaxPosition = 180.0;
 	
 	//Elevator
-	public static final int elevatorTalonMotorChannel = 0;
-	public static final int elevatorEncoderAChannel = 0;
-	public static final int elevatorEncoderBChannel = 0;
+	public static final int elevatorTalonMotorChannel = 18;
+	public static final int elevatorEncoderAChannel = 8;
+	public static final int elevatorEncoderBChannel = 9;
 	public static final boolean elevatorEncoderReversed = false;
 	public static final double elevatorEncoderToFeet = 1.0;
 	public static final double elevatorMinPosition = 0.0;
