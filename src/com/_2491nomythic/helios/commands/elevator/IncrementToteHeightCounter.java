49a6852ca88b647e6f22769851c28c08f816d321
No Mythic Lenovo 2 <2491nomythic@gmail.com>
2015-01-28 14:52:18 -0600

    fixed a bracket in IncrementToteHeightCounter

--- src/com/_2491nomythic/helios/commands/elevator/IncrementToteHeightCounter.java
+++ src/com/_2491nomythic/helios/commands/elevator/IncrementToteHeightCounter.java
@@ -19,13 +19,14 @@ public class IncrementToteHeightCounter extends CommandBase {
     protected void initialize() {
     	if(oi.buttonIncrementer > 0 && oi.buttonIncrementer < 7) {
     	oi.buttonIncrementer++;
-    }
+    	}
     	else if(oi.buttonIncrementer < 0) {
     		oi.buttonIncrementer = 0;
     	}
     	else if(oi.buttonIncrementer > 6) {
     		oi.buttonIncrementer = 6;
     	}
+    }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
