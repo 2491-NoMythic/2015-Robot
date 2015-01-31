6054ef826f8c3ed6373e525c95d504e18b236574
No Mythic Samsung <2491nomythic@gmail.com>
2015-01-31 10:11:13 -0600

    array changes

--- src/com/_2491nomythic/helios/commands/elevator/IncrementToteHeightCounter.java
+++ src/com/_2491nomythic/helios/commands/elevator/IncrementToteHeightCounter.java
@@ -17,6 +17,8 @@ public class IncrementToteHeightCounter extends CommandBase {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	oi.incrementCounter++;
+    	Variables.toteH
     	
     }
 
@@ -26,7 +28,7 @@ public class IncrementToteHeightCounter extends CommandBase {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
