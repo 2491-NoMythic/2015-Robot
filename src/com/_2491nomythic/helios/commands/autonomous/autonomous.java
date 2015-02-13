865e46599a22e15fe8aa194b3b29995f30b4c14b
No Mythic Samsung <2491nomythic@gmail.com>
2015-02-12 20:29:28 -0600

    Worked on autonomous

--- src/com/_2491nomythic/helios/commands/autonomous/autonomous.java
+++ src/com/_2491nomythic/helios/commands/autonomous/autonomous.java
@@ -14,34 +14,17 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class autonomous extends CommandGroup {
     
     public  autonomous() {
-    	addSequential(new RunWithPID(Variables.horizontalAForewardPos));
-    	addSequential(new DriveDistance(Variables.pickupBinDrivePower, Constants.nullX, Variables.pickup1stBinYDistance));
+    	addSequential(new RunWithPID(Variables.binPickup));
+    	addSequential(new DriveDistance(Variables.pickupBinDrivePower, Constants.nullX, Variables.pickupBinYDistance));
     	addSequential(new RunWithPID(Variables.verticalArmPos)); //may be made a parallel if the arm is brought up fast enough
-    	addSequential(new DriveDistance(Variables.driveBackToOrigPosPower, Constants.nullX, -(Variables.pickup1stBinYDistance)));
+    	addSequential(new DriveDistance(Variables.pickupBinDrivePower, Constants.nullX, -(Variables.pickupBinYDistance)));
     	addSequential(new RunWithPID(Variables.recyclingContainerReleasePos));
-    	addSequential(new DriveDistance(Variables.unhookBinPower, Variables.unhookBinXDistance, Variables.unhookBinYDistance));
-    	addSequential(new RunWithPID(Variables.horizontalAForewardPos));
-    	addSequential(new DriveDistance(Variables.pickupBinDrivePower, Constants.nullX, Variables.pickup2ndBinYDistance)); //may be made a parallel if the arm can be brought down fast enough
-    	addSequential(new DriveDistance(Variables.pickupBinDrivePower, Variables.pickup2ndBinXDistance, Constants.nullY));
+    	addSequential(new DriveDistance(Variables.unhookBinPower, Constants.nullX, Variables.unhookBinYDistance));
     	addSequential(new RunWithPID(Variables.verticalArmPos));
-    	addSequential(new DriveDistance(Variables.pickupBinDrivePower, -(Variables.pickup2ndBinXDistance), Constants.nullY)); //may be made a parallel if the arm is brought up fast enough
-    	addSequential(new DriveDistance(Variables.pickupBinDrivePower, Constants.nullX, -(Variables.pickup2ndBinYDistance)));
-    	addSequential(new RunWithPID(Variables.recyclingContainerReleasePos));
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-    	
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
+    	addParallel(new RunWithPID(Variables.binPickup));
+    	addSequential(new DriveDistance(Variables.pickupBinDrivePower, Variables.pickup2ndBinXDistance, Constants.nullY)); //may be made a parallel if the arm can be brought down fast enough
+    	addSequential(new RunWithPID(Variables.verticalArmPos));
+    	addSequential(new DriveDistance(Variables.pickupBinDrivePower, Variables.driveNextToBinXDistance, Constants.nullY)); //may be made a parallel if the arm is brought up fast enough
+    	addSequential(new DriveDistance(Variables.pickupBinDrivePower, Constants.nullX, Variables.driveNextToBinYDistance));
     }
 }
