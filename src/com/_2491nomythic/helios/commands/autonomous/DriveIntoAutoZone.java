4e675500d3de7e6090af438089c60dede10bb3d0
No Mythic Lenovo 2 <2491nomythic@gmail.com>
2015-02-13 21:05:27 -0600

    Autonomous Work
    Added DriveIntoAutoZone

---
+++ src/com/_2491nomythic/helios/commands/autonomous/DriveIntoAutoZone.java
@@ -0,0 +1,20 @@
+package com._2491nomythic.helios.commands.autonomous;
+
+import com._2491nomythic.helios.commands.arm.RunWithPID;
+import com._2491nomythic.helios.commands.drivetrain.DriveDistance;
+import com._2491nomythic.helios.commands.drivetrain.DriveTime;
+import com._2491nomythic.helios.settings.Constants;
+import com._2491nomythic.helios.settings.Variables;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+
+public class DriveIntoAutoZone extends CommandGroup {
+    
+    public  DriveIntoAutoZone() {
+    	addSequential(new DriveTime(0.5, 1.0, 0.0));
+    }
+}
