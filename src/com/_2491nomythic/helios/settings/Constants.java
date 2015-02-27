112736f191f2102cd085d151eee00c0190675761
No Mythic CAD Laptop <2491nomythic@gmail.com>
2015-02-27 13:27:34 -0600

    Changed port mappings

--- src/com/_2491nomythic/helios/settings/Constants.java
+++ src/com/_2491nomythic/helios/settings/Constants.java
@@ -32,18 +32,18 @@ public class Constants {
 	public static final double ArmBrakeMaxSpeed = 10.0; // Max speed of arm to
 														// allow brake to be
 														// engaged
-	public static final int armEncoderAChannel = 15;
-	public static final int armEncoderBChannel = 16;
-	public static final boolean armEncoderReversed = false;
-	public static final double armEncoderToDegrees = 1.0;
+	public static final int armEncoderAChannel = 4;
+	public static final int armEncoderBChannel = 5;
+	public static final boolean armEncoderReversed = true;
+	public static final double armEncoderToDegrees = 0.4;
 	public static final double armMinPosition = 0.0;
 	public static final double armMaxPosition = 180.0;
 	public static final int armHallEffectSensorChannel = 13;
 	
 	// Elevator
 	public static final int elevatorTalonMotorChannel = 14;
-	public static final int elevatorEncoderAChannel = 4;
-	public static final int elevatorEncoderBChannel = 5;
+	public static final int elevatorEncoderAChannel = 15;
+	public static final int elevatorEncoderBChannel = 16;
 	public static final boolean elevatorEncoderReversed = false;
 	public static final double elevatorEncoderToFeet = 1.0;
 	public static final double elevatorMinPosition = 0.0;
