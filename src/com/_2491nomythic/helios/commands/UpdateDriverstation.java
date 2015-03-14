316d3268f0fb0c3d426525404198ea09556e0bad
NoMythic-HP <2491nomythic@gmail.com>
2015-03-14 14:52:55 -0500

    Printed power usage to smartdashboard

--- src/com/_2491nomythic/helios/commands/UpdateDriverstation.java
+++ src/com/_2491nomythic/helios/commands/UpdateDriverstation.java
@@ -13,16 +13,20 @@ public class UpdateDriverstation extends CommandBase {
 	private Timer timer;
 	private double nextRun;
 	private FileData file;
+	private double[] maxValues = new double[8];
+	private boolean maxValuesEnabled = false;
 	
 	public UpdateDriverstation() {
 		requires(extraSensors);
 		timer = new Timer();
 		file = new FileData(Constants.homeDirectory + "SmartDashboard.txt");
+		setRunWhenDisabled(true);
 		SmartDashboard.putNumber("Gyro Sensitivity", file.getDoubleWithDefault("GyroToDegrees", Variables.gyroToDegrees));
 		SmartDashboard.putNumber("Drive Y PID P", file.getDoubleWithDefault("DriveYPIDP", Variables.driveyPID_P));
 		SmartDashboard.putNumber("Drive Y PID I", file.getDoubleWithDefault("DriveYPIDI", Variables.driveyPID_I));
 		SmartDashboard.putNumber("Drive Y PID D", file.getDoubleWithDefault("DriveYPIDD", Variables.driveyPID_D));
 		SmartDashboard.putNumber("Arm Compensation Speed", file.getDoubleWithDefault("ArmCompensationSpeed", Variables.armCompensationMultiplier));
+		SmartDashboard.putBoolean("Show Max Power Usage", false);
 	}
 	
 	protected void initialize() {
@@ -52,22 +56,46 @@ public class UpdateDriverstation extends CommandBase {
 	}
 	
 	private void updateStatus() {
-		/*
-		 * SmartDashboard.putNumber("Power Usage (watts)",
-		 * extraSensors.getPowerDistributionPanel().getTotalPower());
-		 * SmartDashboard.putNumber("Power Usage (amps)",
-		 * extraSensors.getPowerDistributionPanel().getTotalCurrent());
-		 * SmartDashboard.putNumber("Power Used (joules)",
-		 * extraSensors.getPowerDistributionPanel().getTotalEnergy());
-		 */
-		SmartDashboard.putNumber("Current Elevator Target (totes)", Variables.elevatorTarget);
-		if (Variables.elevatorTarget < 0 || Variables.elevatorTarget >= Variables.toteHeight.length) {
-			SmartDashboard.putString("Current Elevator Target (in)", "Manual Control");
+		
+		if (SmartDashboard.getBoolean("Show Max Power Usage")) {
+			if (!maxValuesEnabled) {
+				maxValues = new double[8];
+				maxValuesEnabled = true;
+			}
+			SmartDashboard.putNumber("Front Left Power Usage", updateMaxValue(0, extraSensors.getPowerDistributionPanel().getCurrent(13)));
+			SmartDashboard.putNumber("Front Center Power Usage", updateMaxValue(1, extraSensors.getPowerDistributionPanel().getCurrent(1)));
+			SmartDashboard.putNumber("Front Right Power Usage", updateMaxValue(2, extraSensors.getPowerDistributionPanel().getCurrent(2)));
+			SmartDashboard.putNumber("Back Left Power Usage", updateMaxValue(3, extraSensors.getPowerDistributionPanel().getCurrent(15)));
+			SmartDashboard.putNumber("Back Center Power Usage", updateMaxValue(4, extraSensors.getPowerDistributionPanel().getCurrent(14)));
+			SmartDashboard.putNumber("Back Right Power Usage", updateMaxValue(5, extraSensors.getPowerDistributionPanel().getCurrent(0)));
+			SmartDashboard.putNumber("Arm Power Usage", updateMaxValue(6, extraSensors.getPowerDistributionPanel().getCurrent(3)));
+			SmartDashboard.putNumber("Hook Power Usage", updateMaxValue(7, extraSensors.getPowerDistributionPanel().getCurrent(4)));
 		}
 		else {
-			SmartDashboard.putNumber("Current Elevator Target (in)", Variables.toteHeight[Variables.elevatorTarget]);
+			if (maxValuesEnabled) {
+				maxValuesEnabled = false;
+			}
+			SmartDashboard.putNumber("Front Left Power Usage", extraSensors.getPowerDistributionPanel().getCurrent(13));
+			SmartDashboard.putNumber("Front Center Power Usage", extraSensors.getPowerDistributionPanel().getCurrent(1));
+			SmartDashboard.putNumber("Front Right Power Usage", extraSensors.getPowerDistributionPanel().getCurrent(2));
+			SmartDashboard.putNumber("Back Left Power Usage", extraSensors.getPowerDistributionPanel().getCurrent(15));
+			SmartDashboard.putNumber("Back Center Power Usage", extraSensors.getPowerDistributionPanel().getCurrent(14));
+			SmartDashboard.putNumber("Back Right Power Usage", extraSensors.getPowerDistributionPanel().getCurrent(0));
+			SmartDashboard.putNumber("Arm Power Usage", extraSensors.getPowerDistributionPanel().getCurrent(3));
+			SmartDashboard.putNumber("Hook Power Usage", extraSensors.getPowerDistributionPanel().getCurrent(4));
 		}
-		SmartDashboard.putNumber("Current Elevator Position", elevator.getEncoder().getDistance());
+//		SmartDashboard.putNumber("Power Usage (watts)", extraSensors.getPowerDistributionPanel().getTotalPower());
+//		SmartDashboard.putNumber("Power Usage (amps)", extraSensors.getPowerDistributionPanel().getTotalCurrent());
+//		SmartDashboard.putNumber("Power Used (joules)", extraSensors.getPowerDistributionPanel().getTotalEnergy());
+		
+//		SmartDashboard.putNumber("Current Elevator Target (totes)", Variables.elevatorTarget);
+//		if (Variables.elevatorTarget < 0 || Variables.elevatorTarget >= Variables.toteHeight.length) {
+//			SmartDashboard.putString("Current Elevator Target (in)", "Manual Control");
+//		}
+//		else {
+//			SmartDashboard.putNumber("Current Elevator Target (in)", Variables.toteHeight[Variables.elevatorTarget]);
+//		}
+//		SmartDashboard.putNumber("Current Elevator Position", elevator.getEncoder().getDistance());
 		SmartDashboard.putNumber("Gyro Positon", drivetrain.getGyro().getAngle());
 		SmartDashboard.putNumber("Right Encoder", drivetrain.getRightEncoder().getDistance());
 		SmartDashboard.putNumber("Center Encoder", drivetrain.getCenterEncoder().getDistance());
@@ -113,5 +141,11 @@ public class UpdateDriverstation extends CommandBase {
 			file.set("ArmCompensationSpeed", Double.toString(tmp));
 		}
 	
+	}
+	private double updateMaxValue (int pos, double value) {
+		if(maxValues[pos] < value) {
+			maxValues[pos] = value;
+		}
+		return maxValues[pos];
 	}
 }
