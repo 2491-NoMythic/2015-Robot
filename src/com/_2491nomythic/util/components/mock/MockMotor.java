e4947b8389ff1ba76012e5d6b0d43473b46d88d0
DConnorA <dalvarez@greatriverschool.org>
2015-08-24 22:37:50 -0500

    Made MockMotor

--- src/com/_2491nomythic/util/components/mock/MockMotor.java
+++ src/com/_2491nomythic/util/components/mock/MockMotor.java
@@ -1,5 +1,22 @@
 package com._2491nomythic.util.components.mock;
 
-public class MockMotor {
+import com._2491nomythic.util.components.interfaces.Motor;
 
+public class MockMotor implements Motor {
+	private double speed;
+	
+	public MockMotor(double speed) {
+		this.speed = speed;
+	};
+
+	@Override
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+	@Override
+	public double getSpeed() {
+		return speed;
+	}
+	
 }
