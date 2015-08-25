085596cabe6aa58573725c3dbdefad3e4e03959f
Tellow Krinkle <etang110@gmail.com>
2015-08-24 23:30:34 -0500

    Added HardwareDoubleSolenoid.java

---
+++ src/com/_2491nomythic/util/components/hardware/HardwareDoubleSolenoid.java
@@ -0,0 +1,31 @@
+package com._2491nomythic.util.components.hardware;
+
+import com._2491nomythic.util.components.interfaces.Solenoid;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
+public class HardwareDoubleSolenoid implements Solenoid {
+	
+	private DoubleSolenoid solenoid;
+	
+	public HardwareDoubleSolenoid(int extendChannel, int retractChannel) {
+		this.solenoid = new DoubleSolenoid(extendChannel, retractChannel);
+	}
+
+	@Override
+	public boolean solenoidExtended() {
+		return solenoid.get() == Value.kForward ? true : false;
+	}
+
+	@Override
+	public void extend() {
+		solenoid.set(Value.kForward);
+	}
+
+	@Override
+	public void retract() {
+		solenoid.set(Value.kReverse);
+	}
+	
+}
