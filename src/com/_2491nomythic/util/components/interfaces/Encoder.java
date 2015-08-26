a006a4a1ca6f3e47512002cbab1616adc34b811e
DConnorA <dalvarez@greatriverschool.org>
2015-08-25 22:49:06 -0500

    Made Encoder Interface

---
+++ src/com/_2491nomythic/util/components/interfaces/Encoder.java
@@ -0,0 +1,8 @@
+package com._2491nomythic.util.components.interfaces;
+
+public interface Encoder {
+	double getDistance();
+	double distancePerPulse();
+	void reset();
+	void resetTo();
+}
---
+++ src/com/_2491nomythic/util/components/mock/MockEncoder.java
@@ -0,0 +1,5 @@
+package com._2491nomythic.util.components.mock;
+
+public class MockEncoder {
+
+}
