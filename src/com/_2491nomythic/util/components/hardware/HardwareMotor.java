fd4b31f2a2dc985142fab2ff6f3abd34d4823200
Tellow Krinkle <etang110@gmail.com>
2015-08-26 23:19:44 -0500

    Added easy creation classes to HardwareMotor

--- src/com/_2491nomythic/util/components/hardware/HardwareMotor.java
+++ src/com/_2491nomythic/util/components/hardware/HardwareMotor.java
@@ -2,11 +2,26 @@ package com._2491nomythic.util.components.hardware;
 
 import com._2491nomythic.util.components.interfaces.Motor;
 
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
 
 public class HardwareMotor implements Motor {
 	
 	private final SpeedController controller;
+	
+	public static HardwareMotor createFromCANTalon(int channel) {
+		return new HardwareMotor(new CANTalon(channel));
+	}
+	
+	public static HardwareMotor createFromTalon(int channel) {
+		return new HardwareMotor(new Talon(channel));
+	}
+	
+	public static HardwareMotor createFromJaguar(int channel) {
+		return new HardwareMotor(new Jaguar(channel));
+	}
 	
 	public HardwareMotor(SpeedController controller) {
 		this.controller = controller;
