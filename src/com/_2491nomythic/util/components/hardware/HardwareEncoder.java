3e655c6720331ea171618c090b90df68debc239b
Tellow Krinkle <etang110@gmail.com>
2015-08-26 23:33:42 -0500

    Reset encoders in HardwareEncoder construction
    I doubt this is actually required, but I might as well do it.

--- src/com/_2491nomythic/util/components/hardware/HardwareEncoder.java
+++ src/com/_2491nomythic/util/components/hardware/HardwareEncoder.java
@@ -12,20 +12,24 @@ public class HardwareEncoder implements Encoder {
 	public HardwareEncoder(int channelA, int channelB, boolean inverted, EncodingType encodingType, double distancePerPulse) {
 		encoder = new edu.wpi.first.wpilibj.Encoder(channelA, channelB, inverted, encodingType);
 		encoder.setDistancePerPulse(distancePerPulse);
+		encoder.reset();
 	}
 	
 	public HardwareEncoder(int channelA, int channelB, boolean inverted, double distancePerPulse) {
 		encoder = new edu.wpi.first.wpilibj.Encoder(channelA, channelB, inverted);
 		encoder.setDistancePerPulse(distancePerPulse);
+		encoder.reset();
 	}
 	
 	public HardwareEncoder(int channelA, int channelB, double distancePerPulse) {
 		encoder = new edu.wpi.first.wpilibj.Encoder(channelA, channelB);
 		encoder.setDistancePerPulse(distancePerPulse);
+		encoder.reset();
 	}
 	
 	public HardwareEncoder(int channelA, int channelB) {
 		encoder = new edu.wpi.first.wpilibj.Encoder(channelA, channelB);
+		encoder.reset();
 	}
 
 	@Override
